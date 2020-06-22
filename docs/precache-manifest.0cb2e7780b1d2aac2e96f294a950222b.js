700e3fb2a701119555a1f86ce3a7cc72ab71d000
Patricio Sazo <psazop@gmail.com>
2020-06-22 19:38:50 -0400

    [22.06.2020]Docs Agregado

---
+++ docs/precache-manifest.0cb2e7780b1d2aac2e96f294a950222b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b535ea0968d2422ca6cdd413432b1350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "407a60cd367b282b34cb",
+    "url": "/static/css/main.09203e70.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407a60cd367b282b34cb",
+    "url": "/static/js/main.eec6b28b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
